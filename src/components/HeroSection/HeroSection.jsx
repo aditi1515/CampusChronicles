85721a7f7fa597ff9b8e838c6f7b8b89c44a7c6d
aditi-gemini <aditi@zymo.app>
2023-06-02 21:52:42 +0530

    ui

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
-import "./HeroSection.scss"
-
+import React from "react";
+import "./HeroSection.scss";
+import { blogs } from "../../utils/temp";
+import bgImage from "../../assets/images/Yellow Dotted Portfolio Cover Page.png";
 const HeroSection = () => {
- return (
-  <div className='heroSection'>
-   <h4>Our Blog</h4>
+  const blog = blogs[0];
+  const {
+    title,
+    description,
+    short_description,
+    cover_image,
+    category,
+    author,
+    date,
+    likes,
+    isApproved,
+    slug = "tempropary-slug",
+  } = blog;
+
+  return (
+    <div className="heroSection">
+      {/* <h4>Our Blog</h4>
    <h1>Campus Chronicles</h1>
    <h3> Bridging the Gap Between Students and College Life</h3>
-   <button>Create Blog</button>
-  </div>
- )
-}
+   <button>Create Blog</button> */}
+      <div className="left">
+        <div className="category-date">
+          <span>{category} ,</span>
+          <span>{date}</span>
+        </div>
+        <h1>{title}</h1>
+        <p>{short_description}</p>
+        <button>READ MORE</button>
+      </div>
+      <div className="right">
+        <div className="image-container">
+          <img src={cover_image} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
