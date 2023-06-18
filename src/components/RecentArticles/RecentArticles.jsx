07fa54c175f84339cd6fab15387dbe3467608c81
jatin <jatin26pandey@gmail.com>
2023-06-18 12:19:01 +0530

    backend link updated

--- src/components/RecentArticles/RecentArticles.jsx
+++ src/components/RecentArticles/RecentArticles.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react'
 
-import BlogCard from '../BlogCard/BlogCard'
-import "./RecentArticles.scss"
+import { Splide, SplideSlide } from '@splidejs/react-splide'
+import '@splidejs/react-splide/css'
 import axios from 'axios'
 import { baseURL } from '../../utils/makeRequest'
+import BlogCard from '../BlogCard/BlogCard'
+import "./RecentArticles.scss"
 const RecentArticles = () => {
- const [blogs,setBlogs] = useState([])
+ const [blogs, setBlogs] = useState([])
+
  useEffect(() => {
   const getBlogs = async () => {
    try {
@@ -22,9 +25,25 @@ const RecentArticles = () => {
   <div className='recent-articles'>
    <h2>Recent Articles</h2>
    <div className="blogs-container">
-    {
-     blogs.slice(0,5).map((blog, index) => <BlogCard blog={blog} />)
-    }
+    <Splide aria-label="My Favorite Images" options={{
+     rewind: true,
+     width: '100%',
+     fixedWidth : '25rem',
+     gap: '1rem',
+    }}
+    >
+
+     {
+      blogs.slice(0, 5).map((blog, index) => {
+       return (
+        <SplideSlide>
+         <BlogCard blog={blog} />
+        </SplideSlide>
+       )
+      }
+      )
+     }
+    </Splide>
    </div>
   </div>
  )
