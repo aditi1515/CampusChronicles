6b394aae1f689fd38b13fb11c537a25d55246ea2
jatin <jatin26pandey@gmail.com>
2023-06-19 01:01:02 +0530

    footer logo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import linkedin from '../../assets/images/linkedin.png'
 import './Footer.scss'
+import logo from '../../assets/images/logo.png'
 const Footer = () => {
 
  return (
   <div className='footer'>
-   <div>Logo</div>
+   <div className='logo'>
+    <img src={logo} alt="" />
+   </div>
    <h5>It's not about perfect. It's about effort.</h5>
    <hr />
    <div className='footer-bottom'>
